29f96028d9443d3bcd6e667889252d57ae0a3642
unknown <eriksonc_souza@hotmail.com>
2023-07-22 21:11:45 -0300

    finalizando projeto

---
+++ src/components/cronometro/relogio/index.tsx
@@ -0,0 +1,22 @@
+import style from './relogio.module.scss'
+
+interface Props {
+    tempo: number | undefined
+}
+
+export default function Relogio({ tempo = 0 }: Props) {
+    const minutos = Math.floor(tempo/60);
+    const segundos = tempo % 60;
+    const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, '0');
+    const [seugndoDezena, segundoUnidade] = String(segundos).padStart(2, '0');
+
+    return (
+        <>
+            <span className={style.relogioNumero}>{minutoDezena}</span>
+            <span className={style.relogioNumero}>{minutoUnidade}</span>
+            <span className={style.relogioDivisao}>:</span>
+            <span className={style.relogioNumero}>{seugndoDezena}</span>
+            <span className={style.relogioNumero}>{segundoUnidade}</span>
+        </>
+    )
+}
